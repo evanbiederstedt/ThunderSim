14338ea145063e62b31ca7ea0360b31f05ef122f
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-10-29 23:19:48 -0400

    added script
    Former-commit-id: a371aed413f6b2a934cafd4e0a4c45437b1cdb51

---
+++ norm_sim.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+
+# SimulationParameters.java
+class Simulation():
+    def __init__(self, snv_rate = 0.001, indel_rate = 0.0003, transition_rate = 0.65, hetero_homo_ratio = 2.0, insert_percent = 0.5, indel_mean = 1.0):
+
+
