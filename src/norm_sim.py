d61979a574981babd4e44fe1a498eab36a5c05fc
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-10-31 23:57:39 -0400

    u0

--- src/norm_sim.py
+++ src/norm_sim.py
@@ -4,6 +4,8 @@
 
 # SimulationParameters.java
 class Simulation():
-    def __init__(self, snv_rate = 0.001, indel_rate = 0.0003, transition_rate = 0.65, hetero_homo_ratio = 2.0, insert_percent = 0.5, indel_mean = 1.0):
+    def __init__(self, snv_rate = 0.001, indel_rate = 0.0003, transition_rate = 0.65, hetero_homo_ratio = 2.0, insert_percent = 0.5, indel_mean = 1.0, output_dir, chrlenpath, refpath, prefix="normal"):
 
 
+# Picard: https://pypi.python.org/pypi/pyfaidx
+# htslib/pysam
