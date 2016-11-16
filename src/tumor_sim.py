c4e41d90ec0e31f36503f3e59d68b90a35148210
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-11-15 20:16:30 -0500

    update

--- src/tumor_sim.py
+++ src/tumor_sim.py
@@ -84,8 +84,44 @@ class SimulateRegularSV(object):
             while svtype == 1:
                 svtype = np.random.randint(4)
         print("Start simulating a regular SV, svtype is " + svtype)
+        sistchr = Converter.getSisterChromFromIndex(sisidx)
+        #variant = StructureVariant()
+        if svtype == 0:
+            #variant = TandemDup()
+            variant.sischrom = sistchr
+            variant.name = "TandemDup"
+            # determine duplication times
+            duptimes = np.random.exponential(1) + 1;
+            # ((TandemDup)variant).setDuptimes(duptimes)
+            elif svtype == 1:
+            # deletion
+            # variant = Deletion()
+            # variant.end = site + random.nextInt(seg) + 1;
+            variant.name = "Deletion"
+            # Determine two or one copy loss , Hetero/Homo = hethomratio (only for the first run)
+            if (run == 0 and copy > (1 / (hethomratio + 1)) * hethomratio):
+                
                     
-                    
+    @classmethod
+    def determineSisterChrom(cls, ploidy):
+        sischrIndex = np.random.randint(ploidy)
+        sistchr = None
+        if sischrIndex==0:
+            sistchr = SisterChrom.CHMTIN_A
+        elif sischrIndex==1:
+            sistchr = SisterChrom.CHMITN_B
+        elif sischrIndex==2:
+            sistchr = SisterChrom.CHMITN_C
+        elif sischrIndex==3:
+            sistchr = SisterChrom.CHMITN_D
+        elif sischrIndex==4:
+            sistchr = SisterChrom.CHMITN_E
+        elif sischrIndex==5:
+            sistchr = SisterChrom.CHMITN_F
+        else:
+            print("invalid ploidy. Ploidy should not be greater than 6!!")
+            System.exit(0)
+        return sistchr
                     
 # Converter.java
                     
@@ -100,14 +136,105 @@ import seq_source
 import strand
 import structure_variant
 import tandem_dup
+
+                    
+class Convert(object):
+    @classmethod
+    def getSisterChromIndex(cls, eventChr):
+        eventChrIndex = 0
+        if eventChr==CHMTIN_A:
+            eventChrIndex = 0
+        elif eventChr==CHMITN_B:
+            eventChrIndex = 1
+        elif eventChr==CHMITN_C:
+            eventChrIndex = 2
+        elif eventChr==CHMITN_D:
+            eventChrIndex = 3
+        elif eventChr==CHMITN_E:
+            eventChrIndex = 4
+        elif eventChr==CHMITN_F:
+            eventChrIndex = 5
+        else:
+            print("invalid chromosome")
+        return eventChrIndex
+
+                    
+    @classmethod
+    def convertChrid(cls, cid, startbase, chrnum):
+        # cid is what?
+        if startbase == 0:
+            chrid = cid + 1
+        else:
+            chird = cid
+        if chrid = chrnum - 1:
+            chr = "X"
+        elif chrid = chrnum:
+            chr = "Y"
+        else:
+            chr = str(chrid)
+        return chr
+                    
+    @classmethod
+    def selectChrom(cls, chrnum):
+        chridx = np.random.randint(chrnum) + 1
+        if chridx == chrnum -1:
+            chrid = "X"
+        elif chridx == chrnum:
+            chrid = "Y"
+        else:
+            chrid = str(chridx)
+        return chrid
+             
+    @classmethod
+    def getSisterChromFromIndex(cls, index):
+        srcChr = None
+        # get the sister chrom from the index
+        if index==0:
+           srcChr = SisterChrom.CHMTIN_A
+        elif index==1:
+            srcChr = SisterChrom.CHMITN_B
+        elif index==2:
+            srcChr = SisterChrom.CHMITN_C
+        elif index==3:
+            srcChr = SisterChrom.CHMITN_D
+        elif index==4:
+            srcChr = SisterChrom.CHMITN_E
+        elif index==5:
+            srcChr = SisterChrom.CHMITN_F
+        else:
+            print("invalid chromosome index in ParameterizedSimulator!!")
+        return srcChr
                     
                     
+# 	 * convert vcf snv record to snv
                     
+    @classmethod
+    def convertVCFtoSNV(cls, vcfrecord):
+        fields = vcfrecord.split("\t") # this probably doesn't work
+        pos = Integer.parseInt(fields[1])
+        ref = fields[3]
+        alt = fields[4]
+                    # chromtin = SisterChrom()
+                    # gt = Genotype()
+        genotype = fields[9]
+        if genotype == "0|1":
+            chromtin = SisterChrom.CHMITN_B
+            gt = Genotype.HETERO
+        elif genotype == "1|0":
+            chromtin = SisterChrom.CHMTIN_A
+            gt = Genotype.HETERO
+        else:
+            chromtin = None
+            gt = Genotype.HOMO
+                    # snv = MutRec()
+        snv.pos = pos
+        snv.ref = ref
+        snv.alt = alt
+        snv.sisChrom = chromtin
+        snv.gentype = gt
+        return snv
                     
-                    
-                    
-                    
-                    
+
                     
                     
                     
